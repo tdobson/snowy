e560239946992b30fe148917a6ca11883970df1f
tdobson <hello@tdobson.net>
2023-10-17 20:23:19 +0100

    It works. Hurrah. We've a starting point.

--- index.js
+++ index.js
@@ -3,17 +3,37 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const { sequelize } = require('./models'); // Importing sequelize connection
+const usersRoutes = require('./routes/users');
+const errorHandler = require('./middleware/errorHandler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger');
 
 const app = express();
 
+// Middlewares
 app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('combined'));
 
-// Routes setup here
+// Routes
+app.use('/users', usersRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// API Documentation with Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Error Handler
+app.use(errorHandler);
+
+// Connection to the database and then starting the server
+const PORT = process.env.PORT || 3000;
+sequelize.sync() // If you want to automatically create tables based on your models, you can add { force: true } inside sync(). However, this will also drop existing tables.
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    })
+    .catch(err => {
+      console.log('Error connecting to the database', err);
+    });
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,26 @@ const usersController = require('../controllers/usersController');
 const router = express.Router();
 
 // Define your routes, for instance:
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Returns a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ */
 router.get('/', usersController.getAllUsers);
 router.get('/:id', usersController.getUserById);
 router.post('/', usersController.createUser);
