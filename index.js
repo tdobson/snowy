08ee07e42e723f60676ce20331e8e726718eecf9
tdobson <hello@tdobson.net>
2024-01-16 00:21:24 +0000

    updated main index with mcsroutes and sync db

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const { sequelize } = require('./models'); // Importing sequelize connection
 const userRoutes = require('./routes/userRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const authRoutes = require('./routes/authRoutes');
-
+const mcsRoutes = require('./routes/mcsRoutes');
 
 const errorHandler = require('./middleware/errorHandler');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./config/swagger');
-
 
 const app = express();
 
@@ -22,11 +21,11 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('combined'));
 
-// Routes
-app.use('/users', userRoutes);
-app.use('/projects', projectRoutes);
-app.use('/auth', authRoutes);
-
+// API v1 Routes
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/projects', projectRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/mcs', mcsRoutes);
 
 // API Documentation with Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -36,12 +35,23 @@ app.use(errorHandler);
 
 // Connection to the database and then starting the server
 const PORT = process.env.PORT || 3000;
-sequelize.sync() // If you want to automatically create tables based on your models, you can add { force: true } inside sync(). However, this will also drop existing tables.
-    .then(() => {
-      app.listen(PORT, () => {
+
+// Function to sync database
+const syncDatabase = async () => {
+    try {
+        if (process.env.NODE_ENV === 'development') {
+            await sequelize.sync();
+        } else if (process.env.NODE_ENV === 'production' && process.env.CREATE_TABLES === 'true') {
+            await sequelize.sync();
+        }
+        console.log('Database & tables synced');
+    } catch (err) {
+        console.error('Error syncing database:', err);
+    }
+};
+
+syncDatabase().then(() => {
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-      });
-    })
-    .catch(err => {
-      console.log('Error connecting to the database', err);
     });
+});
