396e2935f041dca7158a36f5c6b171cf28ce3db2
tdobson <hello@tdobson.net>
2024-03-24 15:57:31 +0000

    updated data reference to include custom fields

--- import-scripts-app-script/upowaMapImportScript.gs.js
+++ import-scripts-app-script/upowaMapImportScript.gs.js
@@ -180,7 +180,98 @@ function prepareImportObject(importSheetData){
                 "battery": importSheetData.Total_Costing.Battery, // No direct mapping available; adjust if necessary
                 "batteryCost": importSheetData.Total_Costing.Battery_Sale, // No direct mapping available; might need to derive or set manually
                 "overallCost": importSheetData.Total_Costing.Plot_Total_Inc_Battery,
-                "landlordSupply": ""
+                "landlordSupply": "",
+                customFields: {
+                    entityType: 'plotSpec',
+                    fields: {
+                        Wattage: importSheetData.MAP.Wattage,
+                        Voltage: importSheetData.MAP.Voltage,
+                        MCS_Code: importSheetData.MAP.MCS_Code,
+                        Mids_Out: importSheetData.MAP.Mids_Out,
+                        MID_Gaps: importSheetData.MAP.MID_Gaps,
+                        Panels_off_end: importSheetData.MAP.Panels_off_end,
+                        Shape: importSheetData.MAP.Shape,
+                        NO_Trackers: importSheetData.MAP.NO_Trackers,
+                        String_one: importSheetData.MAP.String_one,
+                        String_two: importSheetData.MAP.String_two,
+                        String_three: importSheetData.MAP.String_three,
+                        String_four: importSheetData.MAP.String_four,
+                        String_five: importSheetData.MAP.String_five,
+                        String_six: importSheetData.MAP.String_six,
+                        String_seven: importSheetData.MAP.String_seven,
+                        String_eight: importSheetData.MAP.String_eight,
+                        IN___ABOVE_ROOF: importSheetData.MAP.IN___ABOVE_ROOF,
+                        OVERSHADING_FACTOR: importSheetData.MAP.OVERSHADING_FACTOR,
+                        ARRAY_Mtwo: importSheetData.MAP.ARRAY_Mtwo,
+                        kWh: importSheetData.MAP.kWh,
+                        COtwo_EQUIVALENT: importSheetData.MAP.COtwo_EQUIVALENT,
+                        Net_kWp: importSheetData.MAP.Net_kWp,
+                        FF_Material: importSheetData.MAP.FF_Material,
+                        PARCEL: importSheetData.MAP.PARCEL,
+                        Plot_Requirement: importSheetData.Total_Costing.Plot_Requirement,
+                        Block___House: importSheetData.Total_Costing.Block___House,
+                        PV_Diverter: importSheetData.Total_Costing.PV_Diverter,
+                        Model_Number: importSheetData.Total_Costing.Model_Number,
+                        Rated_Output__W_: importSheetData.Total_Costing.Rated_Output__W_,
+                        Protective_Device: importSheetData.Total_Costing.Protective_Device,
+                        Finished_Drawing: importSheetData.Total_Costing.Finished_Drawing,
+                        Commissioning_Info_In: importSheetData.Total_Costing.Commissioning_Info_In,
+                        MCS_Completed: importSheetData.Total_Costing.MCS_Completed,
+                        DNO_Document_Completed: importSheetData.Total_Costing.DNO_Document_Completed,
+                        HO_Pack_Completed: importSheetData.Total_Costing.HO_Pack_Completed,
+                        Solar_PV_Kit: importSheetData.Total_Costing.Solar_PV_Kit,
+                        PVD_Material: importSheetData.Total_Costing.PVD_Material,
+                        PVD_Labour: importSheetData.Total_Costing.PVD_Labour,
+                        Electrical_Kit: importSheetData.Total_Costing.Electrical_Kit,
+                        FF_Labour: importSheetData.Total_Costing.FF_Labour,
+                        SF_Material: importSheetData.Total_Costing.SF_Material,
+                        SF_Labour: importSheetData.Total_Costing.SF_Labour,
+                        Solar_Total_Prime: importSheetData.Total_Costing.Solar_Total_Prime,
+                        FF_MU_: importSheetData.Total_Costing.FF_MU_,
+                        Total_Margin: importSheetData.Total_Costing.Total_Margin,
+                        Solar_PV_Kit___Roofing: importSheetData.Total_Costing.Solar_PV_Kit___Roofing,
+                        FF_Material___Sale: importSheetData.Total_Costing.FF_Material___Sale,
+                        FF_Labour___Sale: importSheetData.Total_Costing.FF_Labour___Sale,
+                        DV_MU_: importSheetData.Total_Costing.DV_MU_,
+                        PVD_Sale: importSheetData.Total_Costing.PVD_Sale,
+                        PVD_Material_Sale: importSheetData.Total_Costing.PVD_Material_Sale,
+                        PVD_Labour_Sale: importSheetData.Total_Costing.PVD_Labour_Sale,
+                        SF_MU_: importSheetData.Total_Costing.SF_MU_,
+                        Electrical_Kit___Commissioning: importSheetData.Total_Costing.Electrical_Kit___Commissioning,
+                        SF_Material__Sale_: importSheetData.Total_Costing.SF_Material__Sale_,
+                        SF_Labour__Sale_: importSheetData.Total_Costing.SF_Labour__Sale_,
+                        Plot_Total__Sale_: importSheetData.Total_Costing.Plot_Total__Sale_,
+                        Profit__Sale_: importSheetData.Total_Costing.Profit__Sale_,
+                        BATTERY_MU_: importSheetData.Total_Costing.BATTERY_MU_,
+                        Total_Battery_Margin: importSheetData.Total_Costing.Total_Battery_Margin,
+                        Battery_Sale: importSheetData.Total_Costing.Battery_Sale,
+                        Profit_Battery__SALE_: importSheetData.Total_Costing.Profit_Battery__SALE_,
+                        Margin_At_time_of_Quotation: importSheetData.Total_Costing.Margin_At_time_of_Quotation,
+                        Solar_PV_Kit__Quoted_: importSheetData.Total_Costing.Solar_PV_Kit__Quoted_,
+                        FF_Material__Quoted_: importSheetData.Total_Costing.FF_Material__Quoted_,
+                        FF_Labour__Quoted_: importSheetData.Total_Costing.FF_Labour__Quoted_,
+                        Electrical_Kit__Quoted_: importSheetData.Total_Costing.Electrical_Kit__Quoted_,
+                        PVD_SALE__Quoted_: importSheetData.Total_Costing.PVD_SALE__Quoted_,
+                        PVD_Material__Quoted_: importSheetData.Total_Costing.PVD_Material__Quoted_,
+                        PVD_Labour__Quoted_: importSheetData.Total_Costing.PVD_Labour__Quoted_,
+                        SF_Material__Quoted_: importSheetData.Total_Costing.SF_Material__Quoted_,
+                        SF_Labour__Quoted_: importSheetData.Total_Costing.SF_Labour__Quoted_,
+                        Plot_Total__Quoted_: importSheetData.Total_Costing.Plot_Total__Quoted_,
+                        Profit_When_Quoted: importSheetData.Total_Costing.Profit_When_Quoted,
+                        Todays_Profit__Quoted_: importSheetData.Total_Costing.Todays_Profit__Quoted_,
+                        Total_Battery_margin__Quoted_: importSheetData.Total_Costing.Total_Battery_margin__Quoted_,
+                        Battery__Quoted_: importSheetData.Total_Costing.Battery__Quoted_,
+                        Plot_Total_With_Battery__Quoted_: importSheetData.Total_Costing.Plot_Total_With_Battery__Quoted_,
+                        Battery_Profit__When_Quoted: importSheetData.Total_Costing.Battery_Profit__When_Quoted,
+                        Todays_Profit_With_Battery__Quoted_: importSheetData.Total_Costing.Todays_Profit_With_Battery__Quoted_,
+                        MICROINV: importSheetData.Total_Costing.MICROINV,
+                        SOLIS_SP__CALC_: importSheetData.Total_Costing.SOLIS_SP__CALC_,
+                        SOLIS_threeP__CALC_: importSheetData.Total_Costing.SOLIS_threeP__CALC_,
+                        HUAWEI_HY__CALC_: importSheetData.Total_Costing.HUAWEI_HY__CALC_,
+                        GROWATT_SP: importSheetData.Total_Costing.GROWATT_SP,
+                        GIVENERGY: importSheetData.Total_Costing.GIVENERGY
+                    }
+                }
             },
             plotInstallData: {
                 "plotInstallId": "", // Needs a UUID or a unique identifier; no direct mapping
@@ -232,7 +323,18 @@ function prepareImportObject(importSheetData){
                     "category": ""
                 },
                 "mpanId": "",
-                "pvNumber": importSheetData.Site_Data.Job_code
+                "pvNumber": importSheetData.Site_Data.Job_code,
+                customFields: {
+                    entityType: "site",
+                    fields: {
+                        Surveyor: importSheetData.Site_Data.Surveyor,
+                        Surveyor_email: importSheetData.Site_Data.Surveyor_email,
+                        Surveyor_phone: importSheetData.Site_Data.Surveyor_phone,
+                        SiteAgent: importSheetData.Site_Data.SiteAgent,
+                        SiteAgent_phone: importSheetData.Site_Data.SiteAgent_phone,
+                        SiteAgent_email: importSheetData.Site_Data.SiteAgent_email,
+                    }
+                }
             },
             elevationData: { //shouldn't be specified as not installed yet
                 "plot_install_id": "", // Needs to be derived from plot installation details, so no direct mapping
@@ -270,7 +372,18 @@ function prepareImportObject(importSheetData){
                 "panels_total_cost": "", // No direct mapping available; might need to aggregate panel costs or set manually
                 "roof_kit": importSheetData.MAP.Mounting,
                 "roof_kit_cost": "", // No direct mapping available; might need to derive or set manually
-                "annual_yield": ""
+                "annual_yield": "",
+                customFields: {
+                    entityType: 'elevationSpec',
+                    fields: {
+                        Mounting: importSheetData.MAP.Mounting,
+                        Tile_Type: importSheetData.MAP.Tile_Type,
+                        Elevation_No: importSheetData.MAP.Elevation_No,
+                        Building_Side: importSheetData.MAP.Building_Side,
+                        Input_Roof_Incline: importSheetData.MAP.Input_Roof_Incline,
+                        Input_Variation_from_South: importSheetData.MAP.Input_Variation_from_South
+                    }
+                }
             },
             projectData: {
                 "pvNumber": importSheetData.Site_Data.Job_code,
@@ -297,6 +410,15 @@ function prepareImportObject(importSheetData){
                         "company_role": "Client Contact",
                         "category": "Humans"
                     },
+                    customFields: {
+                        entityType: "project",
+                        fields: {
+                            Total_systems: importSheetData.Site_Data.Total_systems,
+                            Total_Panels: importSheetData.Site_Data.Total_Panels,
+                            Site_kWp: importSheetData.Site_Data.Site_kWp,
+                            Site_cotwo: importSheetData.Site_Data.Site_cotwo
+                        }
+                    }
                 },
                 dnoDetails: {
                     "mpanId": "",
@@ -362,14 +484,18 @@ function main() {
     var conn = Jdbc.getConnection(GLOBAL_DB_URL, GLOBAL_DB_USER, GLOBAL_DB_PASSWORD);
     var importId = insertImportEvent(conn, '', 'Site Log Import', 'Test Import', '4df57691-4d43-4cfb-9338-00e4cfafa63d');
 
-    let rowObject = querySheetsByIndexWithSpecialSheet(queryConfigByIndex) //15-20seconds
-    Logger.log(JSON.stringify(rowObject))
+    let rowObject = querySheetsByIndexWithSpecialSheet(queryConfigByIndex); //15-20seconds
+    Logger.log(JSON.stringify(rowObject));
 
-    let importObject = prepareImportObject(rowObject)
+    if (rowObject.matched === false) {
+        console.log("No matching data found in the 'Total Costing' sheet for the specified row index in the 'MAP' sheet.");
+    }
 
-    Logger.log(JSON.stringify(importObject))
-    // let result = importPlotData(conn, importId, importObject.plotData)
-    //console.log(result)
+    let importObject = prepareImportObject(rowObject);
+    Logger.log(JSON.stringify(importObject));
+
+    // let result = importPlotData(conn, importId, importObject.plotData);
+    // console.log(result);
 }
 
 
