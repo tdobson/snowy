b192cffbdfd3d539a40f3782f377bb01ae4f77ec
tdobson <hello@tdobson.net>
2024-07-31 15:29:05 +0100

    Migrate DnoDetail model to use Sequelize.initialize() method

--- models/dnoDetail.ts
+++ models/dnoDetail.ts
@@ -1,10 +1,8 @@
-// ./models/dnoDetail.js
 // ./models/dnoDetail.ts
-import { Model, DataTypes } from 'sequelize';
-import { DnoDetailInstance, DnoDetailAttributes } from '../types/dnoDetail';
-import sequelize from '../config/sequelize';
+import { Model, DataTypes, Sequelize } from 'sequelize';
+import { DnoDetailAttributes, DnoDetailCreationAttributes } from '../types/dnoDetail';
 
-class DnoDetail extends Model<DnoDetailAttributes, Omit<DnoDetailAttributes, 'dnoDetailsId'>> implements DnoDetailInstance {
+class DnoDetail extends Model<DnoDetailAttributes, DnoDetailCreationAttributes> implements DnoDetailAttributes {
   public dnoDetailsId!: string;
   public mpanPrefix!: number;
   public dnoName!: string;
@@ -15,51 +13,53 @@ class DnoDetail extends Model<DnoDetailAttributes, Omit<DnoDetailAttributes, 'dn
   public internalTel?: string;
   public type?: string;
   public importId?: string;
-}
 
-DnoDetail.init(
-  {
-    dnoDetailsId: {
-      type: DataTypes.CHAR(36),
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    mpanPrefix: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      allowNull: false,
-    },
-    dnoName: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    instanceId: {
-      type: DataTypes.CHAR(36),
-      references: {
-        model: 'sn_instances',
-        key: 'instance_id',
+  public static initialize(sequelize: Sequelize) {
+    this.init(
+      {
+        dnoDetailsId: {
+          type: DataTypes.CHAR(36),
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4,
+        },
+        mpanPrefix: {
+          type: DataTypes.INTEGER,
+          unique: true,
+          allowNull: false,
+        },
+        dnoName: {
+          type: DataTypes.STRING,
+          unique: true,
+          allowNull: false,
+        },
+        instanceId: {
+          type: DataTypes.CHAR(36),
+          references: {
+            model: 'sn_instances',
+            key: 'instance_id',
+          },
+        },
+        address: DataTypes.STRING,
+        emailAddress: DataTypes.STRING,
+        contactNo: DataTypes.STRING,
+        internalTel: DataTypes.STRING,
+        type: DataTypes.STRING,
+        importId: {
+          type: DataTypes.CHAR(36),
+          references: {
+            model: 'sn_import_events',
+            key: 'import_id',
+          },
+        },
       },
-    },
-    address: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    contactNo: DataTypes.STRING,
-    internalTel: DataTypes.STRING,
-    type: DataTypes.STRING,
-    importId: {
-      type: DataTypes.CHAR(36),
-      references: {
-        model: 'sn_import_events',
-        key: 'import_id',
-      },
-    },
-  },
-  {
-    sequelize,
-    modelName: 'DnoDetail',
-    tableName: 'sn_dno_details',
-    underscored: true,
+      {
+        sequelize,
+        modelName: 'DnoDetail',
+        tableName: 'sn_dno_details',
+        underscored: true,
+      }
+    );
   }
-);
+}
 
 export default DnoDetail;
--- types/client.ts
+++ types/client.ts
@@ -48,3 +48,19 @@ interface ClientAttributes {
 interface ClientCreationAttributes extends Optional<ClientAttributes, 'clientId'> {}
 
 export interface ClientInstance extends Model<ClientAttributes, ClientCreationAttributes>, ClientAttributes {}
+import { Model, Optional } from 'sequelize';
+
+export interface ClientAttributes {
+    clientId: string;
+    instanceId: string;
+    clientLegacyNumber?: string;
+    clientName: string;
+    clientAddressId?: string;
+    clientPlotCardRequired?: string;
+    contactId?: string;
+    importId?: string;
+}
+
+export interface ClientCreationAttributes extends Optional<ClientAttributes, 'clientId'> {}
+
+export interface ClientInstance extends Model<ClientAttributes, ClientCreationAttributes>, ClientAttributes {}
