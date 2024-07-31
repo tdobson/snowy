7175619075afb63c5a9d3acec868b726522aad67
tdobson <hello@tdobson.net>
2024-07-31 15:21:45 +0100

    renamed things to .ts

--- types/client.ts
+++ types/client.ts
@@ -32,3 +32,19 @@ export interface ClientCreationAttributes extends Omit<ClientAttributes, 'client
 export interface ClientInstance
   extends Model<ClientAttributes, ClientCreationAttributes>,
     ClientAttributes {}
+import { Model, Optional } from 'sequelize';
+
+interface ClientAttributes {
+  clientId: string;
+  instanceId: string;
+  clientLegacyNumber?: string;
+  clientName: string;
+  clientAddressId?: string;
+  clientPlotCardRequired?: string;
+  contactId?: string;
+  importId?: string;
+}
+
+interface ClientCreationAttributes extends Optional<ClientAttributes, 'clientId'> {}
+
+export interface ClientInstance extends Model<ClientAttributes, ClientCreationAttributes>, ClientAttributes {}
--- types/dnoDetail.ts
+++ types/dnoDetail.ts
@@ -36,3 +36,21 @@ export interface DnoDetailCreationAttributes extends Omit<DnoDetailAttributes, '
 export interface DnoDetailInstance
   extends Model<DnoDetailAttributes, DnoDetailCreationAttributes>,
     DnoDetailAttributes {}
+import { Model, Optional } from 'sequelize';
+
+interface DnoDetailAttributes {
+  dnoDetailsId: string;
+  mpanPrefix: number;
+  dnoName: string;
+  instanceId: string;
+  address?: string;
+  emailAddress?: string;
+  contactNo?: string;
+  internalTel?: string;
+  type?: string;
+  importId?: string;
+}
+
+interface DnoDetailCreationAttributes extends Optional<DnoDetailAttributes, 'dnoDetailsId'> {}
+
+export interface DnoDetailInstance extends Model<DnoDetailAttributes, DnoDetailCreationAttributes>, DnoDetailAttributes {}
