c923a2d3d937bb3a30077c5528df19ee5f7fa555
tdobson <hello@tdobson.net>
2024-08-05 13:47:35 +0100

    chore: integrate prettier with eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
-    // Custom rules here
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prettier/prettier': 'error',
   },
 };
 module.exports = {
